bb2aa32c36ae54c7e20479098a2663d1463ec272
Christopher Jones <chrisjtheonly@gmail.com>
2021-04-29 17:16:38 -0500

    dojo weather

---
+++ script.js
@@ -0,0 +1,14 @@
+function disappear(){
+    document.getElementById('cookies').style.display = "none"; 
+}
+function myFunction() {
+    document.getElementById("degree").value;
+    document.getElementById("1").innerHTML = "75°";
+    document.getElementById("2").innerHTML = "65°";
+    document.getElementById("3").innerHTML = "80°";
+    document.getElementById("4").innerHTML = "66°";
+    document.getElementById("5").innerHTML = "69°";
+    document.getElementById("6").innerHTML = "61°";
+    document.getElementById("7").innerHTML = "78°";
+    document.getElementById("8").innerHTML = "70°";
+}
